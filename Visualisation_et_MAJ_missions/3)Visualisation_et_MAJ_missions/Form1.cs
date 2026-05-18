66156f3fc067b9af7dedd26cefda586c25c0e5c4
Nathan <nathan.wilhelm@unistra.fr>
2026-05-18 09:35:55 +0200

    ajout volet 3) -> prepa a completer dans TDB

---
+++ Visualisation_et_MAJ_missions/3)Visualisation_et_MAJ_missions/Form1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _3_Visualisation_et_MAJ_missions
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
+
+
+// a mettre dans le volet 1 :
+// FormFicheMission fiche = new FormFicheMission("Mars", 1);
+// fiche.ShowDialog();
