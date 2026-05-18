3016c8c074a6a5d00c90ab189c502e25dcec8a1a
Nathan <nathan.wilhelm@unistra.fr>
2026-05-18 10:00:21 +0200

    MAJ volet 3)

--- Visualisation_et_MAJ_missions/3)Visualisation_et_MAJ_missions/Form1.cs
+++ Visualisation_et_MAJ_missions/3)Visualisation_et_MAJ_missions/Form1.cs
@@ -12,9 +12,20 @@ namespace _3_Visualisation_et_MAJ_missions
 {
     public partial class Form1 : Form
     {
+        private SQLiteConnection cx;
+        private string nomPlanete;
+        private int numeroMission;
         public Form1()
         {
             InitializeComponent();
+            this.nomPlanete = nomPlanete;
+            this.numeroMission = numeroMission;
+
+            string chaine = "Data Source=..\\..\\Stargate.db";
+            this.cx = new SQLiteConnection(chaine);
+            this.cx.Open();
+
+            ChargerMission(nomPlanete, numeroMission);
         }
 
         private void Form1_Load(object sender, EventArgs e)
