b34e584d7243f6ad44bc09afa526f843de717cae
Nathan <nathan.wilhelm@unistra.fr>
2026-05-22 12:18:43 +0200

    ajout journal

--- Visualisation_et_MAJ_missions/3)Visualisation_et_MAJ_missions/Form1.Designer.cs
+++ Visualisation_et_MAJ_missions/3)Visualisation_et_MAJ_missions/Form1.Designer.cs
@@ -35,76 +35,96 @@
             this.lblDateRetour = new System.Windows.Forms.Label();
             this.grpFeuilleDeRoute = new System.Windows.Forms.GroupBox();
             this.grpMembresEquipage = new System.Windows.Forms.GroupBox();
+            this.btJournalDeBoard = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // lblNomMission
             // 
             this.lblNomMission.AutoSize = true;
-            this.lblNomMission.Location = new System.Drawing.Point(304, 33);
+            this.lblNomMission.Location = new System.Drawing.Point(608, 63);
+            this.lblNomMission.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
             this.lblNomMission.Name = "lblNomMission";
-            this.lblNomMission.Size = new System.Drawing.Size(64, 13);
+            this.lblNomMission.Size = new System.Drawing.Size(130, 25);
             this.lblNomMission.TabIndex = 0;
             this.lblNomMission.Text = "NomMission";
             // 
             // lblDateDepart
             // 
             this.lblDateDepart.AutoSize = true;
-            this.lblDateDepart.Location = new System.Drawing.Point(54, 120);
+            this.lblDateDepart.Location = new System.Drawing.Point(108, 231);
+            this.lblDateDepart.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
             this.lblDateDepart.Name = "lblDateDepart";
-            this.lblDateDepart.Size = new System.Drawing.Size(35, 13);
+            this.lblDateDepart.Size = new System.Drawing.Size(70, 25);
             this.lblDateDepart.TabIndex = 1;
             this.lblDateDepart.Text = "label1";
             // 
             // lblBudget
             // 
             this.lblBudget.AutoSize = true;
-            this.lblBudget.Location = new System.Drawing.Point(403, 120);
+            this.lblBudget.Location = new System.Drawing.Point(806, 231);
+            this.lblBudget.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
             this.lblBudget.Name = "lblBudget";
-            this.lblBudget.Size = new System.Drawing.Size(35, 13);
+            this.lblBudget.Size = new System.Drawing.Size(70, 25);
             this.lblBudget.TabIndex = 2;
             this.lblBudget.Text = "label2";
             // 
             // lblSoldeApresDepenses
             // 
             this.lblSoldeApresDepenses.AutoSize = true;
-            this.lblSoldeApresDepenses.Location = new System.Drawing.Point(403, 162);
+            this.lblSoldeApresDepenses.Location = new System.Drawing.Point(806, 312);
+            this.lblSoldeApresDepenses.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
             this.lblSoldeApresDepenses.Name = "lblSoldeApresDepenses";
-            this.lblSoldeApresDepenses.Size = new System.Drawing.Size(35, 13);
+            this.lblSoldeApresDepenses.Size = new System.Drawing.Size(70, 25);
             this.lblSoldeApresDepenses.TabIndex = 3;
             this.lblSoldeApresDepenses.Text = "label3";
             // 
             // lblDateRetour
             // 
             this.lblDateRetour.AutoSize = true;
-            this.lblDateRetour.Location = new System.Drawing.Point(54, 162);
+            this.lblDateRetour.Location = new System.Drawing.Point(108, 312);
+            this.lblDateRetour.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
             this.lblDateRetour.Name = "lblDateRetour";
-            this.lblDateRetour.Size = new System.Drawing.Size(35, 13);
+            this.lblDateRetour.Size = new System.Drawing.Size(70, 25);
             this.lblDateRetour.TabIndex = 4;
             this.lblDateRetour.Text = "label4";
             // 
             // grpFeuilleDeRoute
             // 
-            this.grpFeuilleDeRoute.Location = new System.Drawing.Point(57, 208);
+            this.grpFeuilleDeRoute.Location = new System.Drawing.Point(114, 400);
+            this.grpFeuilleDeRoute.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
             this.grpFeuilleDeRoute.Name = "grpFeuilleDeRoute";
-            this.grpFeuilleDeRoute.Size = new System.Drawing.Size(519, 145);
+            this.grpFeuilleDeRoute.Padding = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.grpFeuilleDeRoute.Size = new System.Drawing.Size(1038, 279);
             this.grpFeuilleDeRoute.TabIndex = 5;
             this.grpFeuilleDeRoute.TabStop = false;
             this.grpFeuilleDeRoute.Text = "Feuille de route";
             // 
             // grpMembresEquipage
             // 
-            this.grpMembresEquipage.Location = new System.Drawing.Point(57, 378);
+            this.grpMembresEquipage.Location = new System.Drawing.Point(114, 727);
+            this.grpMembresEquipage.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
             this.grpMembresEquipage.Name = "grpMembresEquipage";
-            this.grpMembresEquipage.Size = new System.Drawing.Size(519, 145);
+            this.grpMembresEquipage.Padding = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.grpMembresEquipage.Size = new System.Drawing.Size(1038, 279);
             this.grpMembresEquipage.TabIndex = 6;
             this.grpMembresEquipage.TabStop = false;
             this.grpMembresEquipage.Text = "Membres de l\'équipage";
             // 
+            // btJournalDeBoard
+            // 
+            this.btJournalDeBoard.Location = new System.Drawing.Point(857, 1065);
+            this.btJournalDeBoard.Name = "btJournalDeBoard";
+            this.btJournalDeBoard.Size = new System.Drawing.Size(295, 113);
+            this.btJournalDeBoard.TabIndex = 7;
+            this.btJournalDeBoard.Text = "Journal de Board >";
+            this.btJournalDeBoard.UseVisualStyleBackColor = true;
+            // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(599, 631);
+            this.ClientSize = new System.Drawing.Size(1198, 1213);
+            this.Controls.Add(this.btJournalDeBoard);
             this.Controls.Add(this.grpMembresEquipage);
             this.Controls.Add(this.grpFeuilleDeRoute);
             this.Controls.Add(this.lblDateRetour);
@@ -112,6 +132,7 @@
             this.Controls.Add(this.lblBudget);
             this.Controls.Add(this.lblDateDepart);
             this.Controls.Add(this.lblNomMission);
+            this.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
@@ -129,6 +150,7 @@
         private System.Windows.Forms.Label lblDateRetour;
         private System.Windows.Forms.GroupBox grpFeuilleDeRoute;
         private System.Windows.Forms.GroupBox grpMembresEquipage;
+        private System.Windows.Forms.Button btJournalDeBoard;
     }
 }
 
---
+++ Visualisation_et_MAJ_missions/3)Visualisation_et_MAJ_missions/Form2.Designer.cs
@@ -0,0 +1,144 @@
+﻿namespace _3_Visualisation_et_MAJ_missions
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.grpEvenement = new System.Windows.Forms.GroupBox();
+            this.grpDepenses = new System.Windows.Forms.GroupBox();
+            this.grpContact = new System.Windows.Forms.GroupBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.grpEvenement.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // grpEvenement
+            // 
+            this.grpEvenement.Controls.Add(this.richTextBox1);
+            this.grpEvenement.Controls.Add(this.button4);
+            this.grpEvenement.Controls.Add(this.button3);
+            this.grpEvenement.Controls.Add(this.button2);
+            this.grpEvenement.Controls.Add(this.button1);
+            this.grpEvenement.Location = new System.Drawing.Point(80, 87);
+            this.grpEvenement.Name = "grpEvenement";
+            this.grpEvenement.Size = new System.Drawing.Size(447, 737);
+            this.grpEvenement.TabIndex = 0;
+            this.grpEvenement.TabStop = false;
+            this.grpEvenement.Text = "Evènements du journal";
+            // 
+            // grpDepenses
+            // 
+            this.grpDepenses.Location = new System.Drawing.Point(579, 87);
+            this.grpDepenses.Name = "grpDepenses";
+            this.grpDepenses.Size = new System.Drawing.Size(638, 485);
+            this.grpDepenses.TabIndex = 1;
+            this.grpDepenses.TabStop = false;
+            this.grpDepenses.Text = "Dépenses effectuées";
+            // 
+            // grpContact
+            // 
+            this.grpContact.Location = new System.Drawing.Point(579, 612);
+            this.grpContact.Name = "grpContact";
+            this.grpContact.Size = new System.Drawing.Size(638, 225);
+            this.grpContact.TabIndex = 2;
+            this.grpContact.TabStop = false;
+            this.grpContact.Text = "Contacts avec les informateurs";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(6, 664);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(80, 67);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(92, 664);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(80, 67);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(361, 664);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(80, 67);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "button3";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(275, 664);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(80, 67);
+            this.button4.TabIndex = 3;
+            this.button4.Text = "button4";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(179, 664);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(90, 67);
+            this.richTextBox1.TabIndex = 4;
+            this.richTextBox1.Text = "";
+            // 
+            // Form2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1261, 920);
+            this.Controls.Add(this.grpContact);
+            this.Controls.Add(this.grpDepenses);
+            this.Controls.Add(this.grpEvenement);
+            this.Name = "Form2";
+            this.Text = "Form2";
+            this.grpEvenement.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox grpEvenement;
+        private System.Windows.Forms.GroupBox grpDepenses;
+        private System.Windows.Forms.GroupBox grpContact;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+    }
+}
