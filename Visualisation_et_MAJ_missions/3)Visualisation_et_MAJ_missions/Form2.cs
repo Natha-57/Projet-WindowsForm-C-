b647c821fb6b15624f7ea3cc3c334ca7da2bf121
Nathan <nathan.wilhelm@unistra.fr>
2026-05-27 10:31:36 +0200

    zarbi git

--- Visualisation_et_MAJ_missions/3)Visualisation_et_MAJ_missions/Form2.cs
+++ Visualisation_et_MAJ_missions/3)Visualisation_et_MAJ_missions/Form2.cs
@@ -34,7 +34,7 @@ namespace _3_Visualisation_et_MAJ_missions
 
             pbHome.SizeMode = PictureBoxSizeMode.CenterImage;
             pbHome.SizeMode = PictureBoxSizeMode.Zoom;
-            pbHome.Image = System.Drawing.Image.FromFile("..\\..\\..\\..\\Images App\\Icones diverses\\home.png");
+            pbHome.Image = System.Drawing.Image.FromFile("..\\..\\..\\..\\Images App\\Icones diverses\\home.png"); 
         }
 
         private void pictureBox1_Click(object sender, EventArgs e)
@@ -179,5 +179,7 @@ namespace _3_Visualisation_et_MAJ_missions
         {
             
         }
+
+        //jevois pas pourquoi on aurait besoin de ça, mais bon, on va laisser ça là au cas où
     }
 }
