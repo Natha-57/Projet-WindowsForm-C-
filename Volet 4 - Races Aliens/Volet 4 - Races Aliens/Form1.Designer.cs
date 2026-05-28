3f18853267b90e05d131073f23db5208883c87c0
Nathan WEISS <nathan.weiss@etu.unistra.fr>
2026-05-28 11:08:34 +0200

    Volet 4 fini + début Volet 5

---
+++ Volet 4 - Races Aliens/Volet 4 - Races Aliens/Form1.Designer.cs
@@ -0,0 +1,244 @@
+﻿namespace Volet_4___Races_Aliens
+{
+    partial class Form_Aliens
+    {
+        private System.ComponentModel.IContainer components = null;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form_Aliens));
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.button_chercher = new System.Windows.Forms.Button();
+            this.button_reset = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.Panel();
+            this.label5 = new System.Windows.Forms.Label();
+            this.comboBox3 = new System.Windows.Forms.ComboBox();
+            this.sqLiteCommand1 = new System.Data.SQLite.SQLiteCommand();
+            this.label4 = new System.Windows.Forms.Label();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button_retour = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(143, 28);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(126, 30);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(383, 28);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(135, 32);
+            this.comboBox1.TabIndex = 1;
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Location = new System.Drawing.Point(608, 28);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(131, 32);
+            this.comboBox2.TabIndex = 2;
+            this.comboBox2.SelectedIndexChanged += new System.EventHandler(this.comboBox2_SelectedIndexChanged);
+            // 
+            // button_chercher
+            // 
+            this.button_chercher.Location = new System.Drawing.Point(1176, 23);
+            this.button_chercher.Name = "button_chercher";
+            this.button_chercher.Size = new System.Drawing.Size(182, 43);
+            this.button_chercher.TabIndex = 3;
+            this.button_chercher.Text = "Rechercher 🔎";
+            this.button_chercher.UseVisualStyleBackColor = true;
+            this.button_chercher.Click += new System.EventHandler(this.button_chercher_Click);
+            // 
+            // button_reset
+            // 
+            this.button_reset.Location = new System.Drawing.Point(1364, 23);
+            this.button_reset.Name = "button_reset";
+            this.button_reset.Size = new System.Drawing.Size(182, 43);
+            this.button_reset.TabIndex = 4;
+            this.button_reset.Text = "Réinitialiser 🔄";
+            this.button_reset.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(21, 31);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(116, 24);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Nom (like) :";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(284, 32);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(93, 24);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Couleur :";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(530, 31);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(72, 24);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Type : ";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.comboBox3);
+            this.groupBox1.Controls.Add(this.button_reset);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.button_chercher);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.textBox1);
+            this.groupBox1.Controls.Add(this.comboBox1);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.comboBox2);
+            this.groupBox1.Font = new System.Drawing.Font("Trebuchet MS", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox1.Location = new System.Drawing.Point(12, 144);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(1555, 88);
+            this.groupBox1.TabIndex = 8;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(760, 32);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(175, 24);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "Planète d\'origine :";
+            // 
+            // comboBox3
+            // 
+            this.comboBox3.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox3.FormattingEnabled = true;
+            this.comboBox3.Location = new System.Drawing.Point(941, 28);
+            this.comboBox3.Name = "comboBox3";
+            this.comboBox3.Size = new System.Drawing.Size(219, 32);
+            this.comboBox3.TabIndex = 8;
+            this.comboBox3.SelectedIndexChanged += new System.EventHandler(this.comboBox3_SelectedIndexChanged);
+            // 
+            // sqLiteCommand1
+            // 
+            this.sqLiteCommand1.CommandText = null;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Trebuchet MS", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(12, 247);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(125, 20);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "0 espèce trouvé";
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
+            this.pictureBox2.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(1555, 126);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox2.TabIndex = 11;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(125, 126);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 10;
+            this.pictureBox1.TabStop = false;
+            // 
+            // button_retour
+            // 
+            this.button_retour.Font = new System.Drawing.Font("Trebuchet MS", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button_retour.Location = new System.Drawing.Point(1323, 48);
+            this.button_retour.Name = "button_retour";
+            this.button_retour.Size = new System.Drawing.Size(244, 60);
+            this.button_retour.TabIndex = 10;
+            this.button_retour.Text = "Revenir sur le tableau de bord 🏠";
+            this.button_retour.UseVisualStyleBackColor = true;
+            this.button_retour.Click += new System.EventHandler(this.button_retour_Click);
+            // 
+            // Form_Aliens
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.ClientSize = new System.Drawing.Size(1575, 818);
+            this.Controls.Add(this.button_retour);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.pictureBox2);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form_Aliens";
+            this.Text = "Races Aliens";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.Button button_chercher;
+        private System.Windows.Forms.Button button_reset;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Panel groupBox1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.ComboBox comboBox3;
+        private System.Data.SQLite.SQLiteCommand sqLiteCommand1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button_retour;
+    }
+}
---
+++ Volet 4 - Races Aliens/Volet 4 - Races Aliens/Form1.cs
@@ -0,0 +1,338 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SQLite;
+
+namespace Volet_4___Races_Aliens
+{
+    public partial class Form_Aliens : Form
+    {
+        private readonly string _cheminBD =
+            System.IO.Path.Combine(
+                AppDomain.CurrentDomain.BaseDirectory,
+                "Stargate.db");
+
+        private readonly string _cheminImages =
+            System.IO.Path.Combine(
+                AppDomain.CurrentDomain.BaseDirectory,
+                "Images");
+
+        private readonly string _cheminImagesPlanetes =
+            System.IO.Path.Combine(
+                AppDomain.CurrentDomain.BaseDirectory,
+                "ImagesPlanetes");
+
+        private DataSet _ds = new DataSet();
+        private FlowLayoutPanel flowRaces;
+
+        public Form_Aliens()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            comboBox1.Items.AddRange(new object[] {
+                "(Toutes)", "Bleu", "Gris", "Marron",
+                "Orange", "Pourpre", "Rose", "Vert", "Violet" });
+            comboBox1.SelectedIndex = 0;
+
+            comboBox2.Items.AddRange(new object[] {
+                "(Tous)", "Alliée", "Ennemie", "Inconnue" });
+            comboBox2.SelectedIndex = 0;
+
+            comboBox3.Items.AddRange(new object[] {
+                "(Toutes)", "Aina", "Aurae", "Jupiter", "Kobaia",
+                "La 9ème planète", "Malaria", "Mars", "Mercure", "Muh",
+                "Neptune", "Saturne", "Sckxyss", "Setna", "Sohia",
+                "Terre", "Uranus", "Vénus", "Origine inconnue" });
+            comboBox3.SelectedIndex = 0;
+
+            flowRaces = new FlowLayoutPanel();
+            flowRaces.Location = new Point(12, 200);
+            flowRaces.Location = new Point(12, 270);
+            flowRaces.Size = new Size(this.ClientSize.Width - 24, this.ClientSize.Height - 280);
+            flowRaces.FlowDirection = FlowDirection.LeftToRight;
+            flowRaces.WrapContents = true;
+            flowRaces.AutoSize = false;
+            flowRaces.AutoScroll = true;
+            flowRaces.Anchor = AnchorStyles.Top | AnchorStyles.Bottom
+                                    | AnchorStyles.Left | AnchorStyles.Right;
+            this.Controls.Add(flowRaces);
+
+            button_chercher.Click += (s, ev) => AppliquerFiltres();
+            button_reset.Click += BtnReset_Click;
+
+            ChargerDonnees();
+            AppliquerFiltres();
+        }
+
+        private void ChargerDonnees()
+        {
+            string sql = @"
+                SELECT
+                    e.id,
+                    e.nom,
+                    e.couleur,
+                    CASE
+                        WHEN a.idEspece IS NOT NULL THEN 'Alliée'
+                        WHEN en.idEspece IS NOT NULL THEN 'Ennemie'
+                        ELSE 'Inconnue'
+                    END AS type,
+                    COALESCE(a.datePremierContact, '') AS datePremierContact,
+                    COALESCE(a.degreBienveillance, '') AS degreBienveillance,
+                    COALESCE(a.instrumentMusique, '') AS instrumentMusique,
+                    COALESCE(en.typeArme, '') AS typeArme,
+                    COALESCE(en.degreAgressivite, '') AS degreAgressivite,
+                    COALESCE(
+                        (SELECT GROUP_CONCAT(h.nomPlanete, ', ')
+                         FROM Habiter h WHERE h.idEspece = e.id),
+                        'Origine inconnue'
+                    ) AS planetes
+                FROM Espece e
+                LEFT JOIN Allie  a  ON e.id = a.idEspece
+                LEFT JOIN Ennemi en ON e.id = en.idEspece
+                ORDER BY e.nom";
+
+            try
+            {
+                using (var conn = new SQLiteConnection(
+                    $"Data Source={_cheminBD};Version=3;"))
+                {
+                    conn.Open();
+                    _ds.Tables.Add("Races");
+                    using (var da = new SQLiteDataAdapter(sql, conn))
+                        da.Fill(_ds.Tables["Races"]);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(
+                    $"Impossible de charger la base :\n\n{ex.Message}",
+                    "Erreur", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void AppliquerFiltres()
+        {
+            if (_ds.Tables["Races"] == null) return;
+
+            string nom = textBox1.Text.Trim().ToLower();
+            string couleur = comboBox1.SelectedItem?.ToString() ?? "(Toutes)";
+            string type = comboBox2.SelectedItem?.ToString() ?? "(Tous)";
+            string planete = comboBox3.SelectedItem?.ToString() ?? "(Toutes)";
+
+            var lignes = _ds.Tables["Races"].AsEnumerable().Where(r =>
+            {
+                bool okNom = string.IsNullOrEmpty(nom) ||
+                                 r["nom"].ToString().ToLower().Contains(nom);
+                bool okCouleur = couleur == "(Toutes)" ||
+                                 r["couleur"].ToString() == couleur;
+                bool okType = type == "(Tous)" ||
+                                 r["type"].ToString() == type;
+                bool okPlanete = planete == "(Toutes)" ||
+                                 (planete == "Origine inconnue" &&
+                                  r["planetes"].ToString() == "Origine inconnue") ||
+                                 r["planetes"].ToString().Contains(planete);
+                return okNom && okCouleur && okType && okPlanete;
+            }).ToList();
+
+            flowRaces.SuspendLayout();
+            flowRaces.Controls.Clear();
+
+            foreach (var row in lignes)
+                flowRaces.Controls.Add(CreerCarteRace(row));
+
+            flowRaces.ResumeLayout();
+
+            label4.Text = $"{lignes.Count} espèce(s) trouvée(s)";
+        }
+
+        private Panel CreerCarteRace(DataRow row)
+        {
+            string typeRace = row["type"].ToString();
+            bool allie = typeRace == "Alliée";
+            bool ennemi = typeRace == "Ennemie";
+
+            Color bordure = allie ? Color.FromArgb(0, 150, 70)
+                          : ennemi ? Color.FromArgb(190, 30, 30)
+                                   : Color.FromArgb(120, 130, 150);
+
+            // --- Carte ---
+            Panel carte = new Panel();
+            carte.Size = new Size(160, 212);
+            carte.Margin = new Padding(5);
+            carte.BackColor = Color.White;
+            carte.Cursor = Cursors.Hand;
+            carte.Tag = row;
+            carte.Paint += (s, e) => e.Graphics.DrawRectangle(
+                                  new Pen(bordure, 2), 1, 1,
+                                  carte.Width - 3, carte.Height - 3);
+
+            // --- Panel image ---
+            Panel pnlImg = new Panel();
+            pnlImg.Size = new Size(130, 110);
+            pnlImg.Location = new Point(15, 20);
+            pnlImg.BackColor = Color.White;
+            pnlImg.BorderStyle = BorderStyle.None;
+
+            PictureBox img = new PictureBox();
+            img.Size = new Size(130, 110);
+            img.Location = new Point(0, 0);
+            img.SizeMode = PictureBoxSizeMode.Zoom;
+            img.BackColor = Color.White;
+
+            string cheminImg = System.IO.Path.Combine(
+                                   _cheminImages,
+                                   row["nom"].ToString() + ".png");
+
+            if (System.IO.File.Exists(cheminImg))
+            {
+                img.Image = Image.FromFile(cheminImg);
+            }
+            else
+            {
+                img.BackColor = CouleurEspece(row["couleur"].ToString());
+                string initTxt = row["nom"].ToString().Length >= 2
+                                 ? row["nom"].ToString().Substring(0, 2).ToUpper()
+                                 : row["nom"].ToString().ToUpper();
+                img.Paint += (s, e) =>
+                {
+                    using (Font f = new Font("Trebuchet MS", 14F, FontStyle.Bold))
+                    using (StringFormat sf = new StringFormat
+                    {
+                        Alignment = StringAlignment.Center,
+                        LineAlignment = StringAlignment.Center
+                    })
+                    {
+                        e.Graphics.DrawString(initTxt, f, Brushes.White,
+                            new RectangleF(0, 0, img.Width, img.Height), sf);
+                    }
+                };
+            }
+            pnlImg.Controls.Add(img);
+
+            // --- Badge type ---
+            Label badge = new Label();
+            badge.Text = allie ? "ALLIÉE" : ennemi ? "ENNEMIE" : "?";
+            badge.Font = new Font("Trebuchet MS", 7.5F, FontStyle.Bold);
+            badge.ForeColor = Color.White;
+            badge.BackColor = bordure;
+            badge.TextAlign = ContentAlignment.MiddleCenter;
+            badge.Location = new Point(0, 0);
+            badge.Size = new Size(65, 18);
+            badge.AutoSize = false;
+
+            // --- Nom ---
+            Label lblNom = new Label();
+            lblNom.Text = row["nom"].ToString();
+            lblNom.Font = new Font("Trebuchet MS", 10F, FontStyle.Bold);
+            lblNom.ForeColor = Color.FromArgb(20, 40, 80);
+            lblNom.TextAlign = ContentAlignment.MiddleCenter;
+            lblNom.Location = new Point(0, 133);
+            lblNom.Size = new Size(160, 22);
+            lblNom.AutoSize = false;
+
+            // --- Couleur ---
+            Label lblCouleur = new Label();
+            lblCouleur.Text = row["couleur"].ToString();
+            lblCouleur.Font = new Font("Trebuchet MS", 9F);
+            lblCouleur.ForeColor = Color.FromArgb(90, 100, 120);
+            lblCouleur.TextAlign = ContentAlignment.MiddleCenter;
+            lblCouleur.Location = new Point(0, 157);
+            lblCouleur.Size = new Size(160, 18);
+            lblCouleur.AutoSize = false;
+
+            // --- Planètes ---
+            Label lblPlanete = new Label();
+            lblPlanete.Text = row["planetes"].ToString();
+            lblPlanete.Font = new Font("Trebuchet MS", 8.5F, FontStyle.Italic);
+            lblPlanete.ForeColor = Color.FromArgb(110, 120, 140);
+            lblPlanete.TextAlign = ContentAlignment.TopCenter;
+            lblPlanete.Location = new Point(0, 177);
+            lblPlanete.Size = new Size(160, 30);
+            lblPlanete.AutoSize = false;
+
+            carte.Controls.AddRange(new Control[] {
+        pnlImg, badge, lblNom, lblCouleur, lblPlanete });
+
+            // Hover
+            carte.MouseEnter += (s, e) => carte.BackColor = Color.FromArgb(235, 242, 255);
+            carte.MouseLeave += (s, e) => carte.BackColor = Color.White;
+
+            // Clic → détail sur tous les contrôles de la carte
+            EventHandler clic = (s, e) => AfficherDetail(row);
+            carte.Click += clic;
+            pnlImg.Click += clic;
+            img.Click += clic;
+            lblNom.Click += clic;
+            lblCouleur.Click += clic;
+            lblPlanete.Click += clic;
+            badge.Click += clic;
+
+            return carte;
+        }
+
+        private void AfficherDetail(DataRow row)
+        {
+            using (FormDetail fd = new FormDetail(row, _cheminImages, _cheminImagesPlanetes))
+                fd.ShowDialog(this);
+        }
+
+        private void BtnReset_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "";
+            comboBox1.SelectedIndex = 0;
+            comboBox2.SelectedIndex = 0;
+            comboBox3.SelectedIndex = 0;
+            AppliquerFiltres();
+        }
+
+        private Color CouleurEspece(string couleur)
+        {
+            switch (couleur)
+            {
+                case "Bleu": return Color.FromArgb(40, 100, 200);
+                case "Gris": return Color.FromArgb(120, 130, 145);
+                case "Marron": return Color.FromArgb(130, 80, 40);
+                case "Orange": return Color.FromArgb(220, 120, 20);
+                case "Pourpre": return Color.FromArgb(130, 0, 130);
+                case "Rose": return Color.FromArgb(210, 80, 130);
+                case "Vert": return Color.FromArgb(30, 140, 70);
+                case "Violet": return Color.FromArgb(100, 60, 180);
+                default: return Color.FromArgb(100, 110, 130);
+            }
+        }
+
+        private string NiveauLabel(string code)
+        {
+            switch (code)
+            {
+                case "A": return "A — Très élevé";
+                case "B": return "B — Élevé";
+                case "C": return "C — Moyen";
+                case "D": return "D — Faible";
+                case "E": return "E — Très faible";
+                case "F": return "F — Minimal";
+                default: return code;
+            }
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e) { }
+        private void label2_Click(object sender, EventArgs e) { }
+        private void comboBox3_SelectedIndexChanged(object sender, EventArgs e) { }
+        private void comboBox2_SelectedIndexChanged(object sender, EventArgs e) { }
+        private void button_chercher_Click(object sender, EventArgs e) { AppliquerFiltres(); }
+
+        private void button_retour_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
